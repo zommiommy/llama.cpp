0b41e83248d0ae86a73bb710b09d7c7e10187246
Prajwal B Mehendarkar <prajwal.b.mehendarkar@ibm.com>
2025-04-12 21:03:39 +0530

    common : Define cache directory on AIX (#12915)

--- examples/rpc/rpc-server.cpp
+++ examples/rpc/rpc-server.cpp
@@ -126,7 +126,7 @@ static std::string fs_get_cache_directory() {
     if (getenv("LLAMA_CACHE")) {
         cache_directory = std::getenv("LLAMA_CACHE");
     } else {
-#if defined(__linux__) || defined(__FreeBSD__)
+#if defined(__linux__) || defined(__FreeBSD__) || defined(_AIX)
         if (std::getenv("XDG_CACHE_HOME")) {
             cache_directory = std::getenv("XDG_CACHE_HOME");
         } else {
