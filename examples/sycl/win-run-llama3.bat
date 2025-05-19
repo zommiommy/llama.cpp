72103f72898e162c12ed83597ab8802de76275bd
Alberto Cabrera Pérez <alberto.cabrera@codeplay.com>
2025-05-19 14:38:20 +0100

    sycl : backend documentation review (#13544)
    * sycl: reviewing and updating docs
    
    * Updates Runtime error codes
    
    * Improves OOM troubleshooting entry
    
    * Added a llama 3 sample
    
    * Updated supported models
    
    * Updated releases table

--- examples/sycl/win-run-llama3.bat
+++ examples/sycl/win-run-llama3.bat
@@ -6,4 +6,4 @@ set INPUT2="Building a website can be done in 10 simple steps:\nStep 1:"
 @call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat" intel64 --force
 
 
-.\build\bin\llama-cli.exe -m models\llama-2-7b.Q4_0.gguf -p %INPUT2% -n 400 -e -ngl 99 -s 0
+.\build\bin\llama-cli.exe -m models\Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf -p %INPUT2% -n 400 -e -ngl 99
