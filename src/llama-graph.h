89e07d12ad294892a188bcf468797dc37f6e3903
Georgi Gerganov <ggerganov@gmail.com>
2025-06-27 21:42:02 +0300

    graph : make llm_graph_context destructor virtual (#14410)
    ggml-ci

--- src/llama-graph.h
+++ src/llama-graph.h
@@ -475,6 +475,7 @@ struct llm_graph_context {
     std::unique_ptr<llm_graph_result> res;
 
     llm_graph_context(const llm_graph_params & params);
+    virtual ~llm_graph_context() = default;
 
     void cb(ggml_tensor * cur, const char * name, int il) const;
 
