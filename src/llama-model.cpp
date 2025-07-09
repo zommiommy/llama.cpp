ffe66af1728192f333df2f7321b82aed41b703ac
Sigbjørn Skjæret <sigbjorn.skjaeret@scala.com>
2025-07-09 10:19:50 +0200

    llama : remove unintended whitespace (#14592)

--- src/llama-model.cpp
+++ src/llama-model.cpp
@@ -4555,7 +4555,7 @@ bool llama_model::load_tensors(llama_model_loader & ml) {
                     // output
                     output = create_tensor(tn(LLM_TENSOR_OUTPUT, "weight"), {hidden_size, n_vocab}, TENSOR_NOT_REQUIRED);
                     output_norm = create_tensor(tn(LLM_TENSOR_OUTPUT_NORM, "weight"), {hidden_size}, 0);
-                    
+
                     // if output is NULL, init from the input tok embed
                     if (output == NULL) {
                         output = create_tensor(tn(LLM_TENSOR_TOKEN_EMBD, "weight"), {hidden_size, n_vocab}, TENSOR_DUPLICATED);
