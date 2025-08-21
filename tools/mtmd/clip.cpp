53eae766d0332d09902b50e39aea49968337888d
Michael Giba <michaelgiba@gmail.com>
2025-08-21 05:06:46 -0500

    ci : fix -Werror=return-type in clip.cpp so ci/run.sh can run without issue (#15221)
    * Fix -Werror=return-type so ci/run.sh can run
    
    * Update tools/mtmd/clip.cpp
    
    Co-authored-by: Diego Devesa <slarengh@gmail.com>
    
    * Remove false now that we have abort
    
    ---------
    
    Co-authored-by: Diego Devesa <slarengh@gmail.com>

--- tools/mtmd/clip.cpp
+++ tools/mtmd/clip.cpp
@@ -3581,10 +3581,10 @@ bool clip_image_preprocess(struct clip_ctx * ctx, const clip_image_u8 * img, str
         }
 
         return true;
-
+    } else {
+        GGML_ABORT("Unknown image preprocessing type");
     }
 
-    GGML_ASSERT(false && "Unknown image preprocessing type");
 }
 
 ggml_tensor * clip_get_newline_tensor(const struct clip_ctx * ctx) {
