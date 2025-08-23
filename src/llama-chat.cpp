9f697c45a5efcc8799c06b6dac416aec1b26ea3d
LaffeyNyaa <112215776+LaffeyNyaa@users.noreply.github.com>
2025-08-23 16:38:30 +0800

    chat : fix debug build assertion in trim function (#15520)

--- src/llama-chat.cpp
+++ src/llama-chat.cpp
@@ -16,10 +16,10 @@
 static std::string trim(const std::string & str) {
     size_t start = 0;
     size_t end = str.size();
-    while (start < end && isspace(str[start])) {
+    while (start < end && isspace(static_cast<unsigned char>(str[start]))) {
         start += 1;
     }
-    while (end > start && isspace(str[end - 1])) {
+    while (end > start && isspace(static_cast<unsigned char>(str[end - 1]))) {
         end -= 1;
     }
     return str.substr(start, end - start);
