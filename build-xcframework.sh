5355ff62f9a857f8ecdc3547ec238603e638f961
Adrien Gallouët <angt@huggingface.co>
2025-09-26 12:39:35 +0200

    build : fix build-ios-device (#16257)
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- build-xcframework.sh
+++ build-xcframework.sh
@@ -422,6 +422,7 @@ echo "Building for iOS devices..."
 cmake -B build-ios-device -G Xcode \
     "${COMMON_CMAKE_ARGS[@]}" \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=${IOS_MIN_OS_VERSION} \
+    -DCMAKE_SYSTEM_NAME=iOS \
     -DCMAKE_OSX_SYSROOT=iphoneos \
     -DCMAKE_OSX_ARCHITECTURES="arm64" \
     -DCMAKE_XCODE_ATTRIBUTE_SUPPORTED_PLATFORMS=iphoneos \
