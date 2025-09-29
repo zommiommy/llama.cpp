89dcef2d73c4a2c55a0abb512ec0fff0f28094eb
Sigbjørn Skjæret <sigbjorn.skjaeret@scala.com>
2025-09-29 11:09:00 +0200

    ggml : check cuda and metal argsort limits and add test (#16323)
    * check cuda argsort limits and add test
    
    * add metal check

--- tests/test-backend-ops.cpp
+++ tests/test-backend-ops.cpp
@@ -6567,6 +6567,7 @@ static std::vector<std::unique_ptr<test_case>> make_test_cases_eval() {
         test_cases.emplace_back(new test_argsort(GGML_TYPE_F32, {16, 10, 10, 10}, order));
         test_cases.emplace_back(new test_argsort(GGML_TYPE_F32, {60, 10, 10, 10}, order)); // qwen
         test_cases.emplace_back(new test_argsort(GGML_TYPE_F32, {1024, 1, 1, 1}, order));
+        test_cases.emplace_back(new test_argsort(GGML_TYPE_F32, {16384, 1, 1, 1}, order)); // bailingmoe2 (group selection)
     }
 
     for (ggml_scale_mode mode : {GGML_SCALE_MODE_NEAREST, GGML_SCALE_MODE_BILINEAR}) {
