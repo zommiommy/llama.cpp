1215bf3329591f3fa5c26c8ffac419e9b59c3522
Shagun Bera <141054835+notV3NOM@users.noreply.github.com>
2025-10-30 21:17:31 +0530

    common: fix typo in cli help text (#16864)

--- common/arg.cpp
+++ common/arg.cpp
@@ -3203,7 +3203,7 @@ common_params_context common_params_parser_init(common_params & params, llama_ex
     ).set_examples({LLAMA_EXAMPLE_IMATRIX}));
     add_opt(common_arg(
         {"--parse-special"},
-        string_format("prase special tokens (chat, tool, etc) (default: %s)", params.parse_special ? "true" : "false"),
+        string_format("parse special tokens (chat, tool, etc) (default: %s)", params.parse_special ? "true" : "false"),
         [](common_params & params) {
             params.parse_special = true;
         }
