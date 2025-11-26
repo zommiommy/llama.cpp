3c27257edb74304ce900028ec463ee513829f9fd
Adrien Gallouët <angt@huggingface.co>
2025-11-26 14:15:21 +0100

    cmake : use EXCLUDE_FROM_ALL to avoid patch-boringssl.cmake (#17520)
    We have to separate the code path starting 3.28 because
    `FetchContent_Populate` is now deprecated and will be completely removed
    in a future version.
    
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- vendor/cpp-httplib/patch-boringssl.cmake
+++
@@ -1,6 +0,0 @@
-# Remove bssl
-file(READ "CMakeLists.txt" content)
-string(REPLACE "add_executable(bssl" "#add_executable(bssl" content "${content}")
-string(REPLACE "target_link_libraries(bssl" "#target_link_libraries(bssl" content "${content}")
-string(REPLACE "install(TARGETS bssl" "#install(TARGETS bssl" content "${content}")
-file(WRITE "CMakeLists.txt" "${content}")
