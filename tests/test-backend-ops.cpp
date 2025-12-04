b9125460ceaa44cf4340c281e51ddc25d4409c6e
Piotr Wilkin (ilintar) <piotr.wilkin@syndatis.com>
2025-12-04 22:19:51 +0100

    Add support for CUMSUM and TRI for CUDA. (#17584)
    * Add support for CUMSUM and TRI for CUDA.
    
    * Minor optimizations.
    
    * Correct warp_prefix_inclusive_sum in float2 variant to return float2
    
    * Optimize TRI
    
    * Whitespace
    
    * Fix strides.
    
    * Implement double loop
    
    * Whitespace
    
    * Fix HIP compilation bugs
    
    * Optimizations + big case performance tests
    
    * Implement using CUB with fallback to custom kernel
    
    * Remove error message.
    
    * Fixes from code review
    
    * Comment out CPU-unsupported F16/BF16 cases to fix CI
    
    * Fine, you win :P
    
    * Fix last cast, use NO_DEVICE_CODE and GGML_UNUSED_VARS
    
    * Vary warp-size based on physical warp size
    
    * Add GGML_UNUSED_VARS in tri as well
    
    * Use constexpr and call prefix_inclusive with warp_size template param
    
    * Update ggml/src/ggml-cuda/cumsum.cu
    
    Co-authored-by: Johannes Gäßler <johannesg@5d6.de>
    
    * Apply suggestions from code review
    
    Co-authored-by: Johannes Gäßler <johannesg@5d6.de>
    
    * Change to tid % warp_size
    
    * Fix strides; hardcode mask; add ggml_lane_mask_t
    
    * Missing renames, remove unused get_warp_mask(), explicit calls to ggml_cuda_info()
    
    * Too hasty...
    
    ---------
    
    Co-authored-by: Johannes Gäßler <johannesg@5d6.de>

--- tests/test-backend-ops.cpp
+++ tests/test-backend-ops.cpp
@@ -7725,6 +7725,7 @@ static std::vector<std::unique_ptr<test_case>> make_test_cases_eval() {
     test_cases.emplace_back(new test_cumsum(GGML_TYPE_F32, { 10, 5, 4, 3 }));
     test_cases.emplace_back(new test_cumsum(GGML_TYPE_F32, { 127, 5, 4, 3 }));
     test_cases.emplace_back(new test_cumsum(GGML_TYPE_F32, { 128, 5, 4, 3 }));
+    test_cases.emplace_back(new test_cumsum(GGML_TYPE_F32, { 128, 128, 4, 4 }));
     test_cases.emplace_back(new test_cumsum(GGML_TYPE_F32, { 255, 5, 4, 3 }));
     test_cases.emplace_back(new test_cumsum(GGML_TYPE_F32, { 256, 5, 4, 3 }));
     test_cases.emplace_back(new test_cumsum(GGML_TYPE_F32, { 511, 5, 4, 3 }));
@@ -7953,6 +7954,13 @@ static std::vector<std::unique_ptr<test_case>> make_test_cases_perf() {
 
     test_cases.emplace_back(new test_solve_tri(GGML_TYPE_F32, { 64, 64, 4, 2 }, { 6, 64, 4, 2 }));
     test_cases.emplace_back(new test_solve_tri(GGML_TYPE_F32, { 128, 128, 4, 1 }, { 8, 128, 4, 1 }));
+
+    test_cases.emplace_back(new test_tri(GGML_TRI_TYPE_LOWER, GGML_TYPE_F32, { 256, 256, 4, 4 }));
+    test_cases.emplace_back(new test_tri(GGML_TRI_TYPE_UPPER_DIAG, GGML_TYPE_F32, { 1024, 1024, 8, 4 }));
+
+    test_cases.emplace_back(new test_cumsum(GGML_TYPE_F32, { 128, 128, 4, 4 }));
+    test_cases.emplace_back(new test_cumsum(GGML_TYPE_F32, { 2048, 16, 5, 4 }));
+    test_cases.emplace_back(new test_cumsum(GGML_TYPE_F32, { 20000, 10, 4, 1 }));
 
     for (int bs : {1, 2, 3, 4, 5, 8, 512}) {
         for (ggml_type type_a : all_types) {
