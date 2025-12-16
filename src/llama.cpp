805c26745dc1595224702041634cc53835fc40c8
Johannes Gäßler <johannesg@5d6.de>
2025-12-17 00:50:12 +0100

    llama-fit-params: force disable mlock (#18103)

--- src/llama.cpp
+++ src/llama.cpp
@@ -71,8 +71,9 @@ static std::vector<llama_device_memory_data> llama_get_device_memory_data(
     }, &ud);
 
     llama_model_params mparams_copy = *mparams;
-    mparams_copy.no_alloc = true;
-    mparams_copy.use_mmap = false;
+    mparams_copy.no_alloc  = true;
+    mparams_copy.use_mmap  = false;
+    mparams_copy.use_mlock = false;
 
     llama_model * model = llama_model_load_from_file(path_model, mparams_copy);
     if (model == nullptr) {
