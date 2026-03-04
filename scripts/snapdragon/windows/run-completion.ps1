e4b48e275f37c39a5e90a4faa81916fd22948d7d
Todor Boinovski <todorb@qti.qualcomm.com>
2026-03-04 15:04:59 -0800

    hexagon: add llama-completion runner script (#20095)

--- scripts/snapdragon/windows/run-completion.ps1
+++ scripts/snapdragon/windows/run-completion.ps1
@@ -46,8 +46,8 @@ if ($null -ne $env:NDEV) {
 
 $env:ADSP_LIBRARY_PATH="$basedir\lib"
 
-& "$basedir\bin\llama-cli.exe" `
+& "$basedir\bin\llama-completion.exe" `
     --no-mmap -m $basedir\..\..\gguf\$model `
     --poll 1000 -t 6 --cpu-mask 0xfc --cpu-strict 1 `
-    --ctx-size 8192 --ubatch-size 128 -fa on `
-    -ngl 99 --device $device $cli_opts
+    --ctx-size 8192 --batch-size 128 -fa on `
+    -ngl 99 -no-cnv --device $device $cli_opts
