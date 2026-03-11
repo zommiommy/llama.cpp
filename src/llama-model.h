5ed0b62368e972e18084f632f60a7110be501344
Sigbjørn Skjæret <sigbjorn.skjaeret@scala.com>
2026-03-11 21:18:29 +0100

    llama : whitespace cleanup (#20422)

--- src/llama-model.h
+++ src/llama-model.h
@@ -299,7 +299,7 @@ struct llama_layer {
     struct ggml_tensor * ffn_gate_exps_s   = nullptr;
     struct ggml_tensor * ffn_down_exps_s   = nullptr;
     struct ggml_tensor * ffn_up_exps_s     = nullptr;
-  
+
     // ff MoE latent proj
     struct ggml_tensor * ffn_latent_down = nullptr;
     struct ggml_tensor * ffn_latent_up   = nullptr;
