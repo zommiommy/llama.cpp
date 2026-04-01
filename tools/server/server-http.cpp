4db5cd06665f261d8002f98a4cad1a1b06b68b1a
Aleksander Grygier <aleksander.grygier@gmail.com>
2026-04-01 21:32:15 +0200

    server: Bypass API Key validation for WebUI static bundle assets (#21269)
    * fix: Bypass API Key validation for static bundle assets
    
    * refactor: All bypassed routes in `public_endpoints`
    
    * test: Update static assets API Key test

--- tools/server/server-http.cpp
+++ tools/server/server-http.cpp
@@ -143,7 +143,11 @@ bool server_http_context::init(const common_params & params) {
             "/v1/health",
             "/models",
             "/v1/models",
-            "/api/tags"
+            "/api/tags",
+            "/",
+            "/index.html",
+            "/bundle.js",
+            "/bundle.css",
         };
 
         // If API key is not set, skip validation
@@ -151,8 +155,8 @@ bool server_http_context::init(const common_params & params) {
             return true;
         }
 
-        // If path is public or is static file, skip validation
-        if (public_endpoints.find(req.path) != public_endpoints.end() || req.path == "/") {
+        // If path is public or static file, skip validation
+        if (public_endpoints.find(req.path) != public_endpoints.end()) {
             return true;
         }
 
