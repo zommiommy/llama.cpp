ee15c85dcfcce5ab9a3ef731a828d5c6e8597590
Piotr Wilkin (ilintar) <piotr.wilkin@syndatis.com>
2026-04-03 13:40:41 +0200

    fix: remove stale assert (#21369)

--- src/llama-vocab.cpp
+++ src/llama-vocab.cpp
@@ -3712,9 +3712,7 @@ int llama_vocab::max_token_len() const {
 
 int llama_vocab::find_bpe_rank(const std::string & token_left, const std::string & token_right) const {
     GGML_ASSERT(token_left.find(' ')   == std::string::npos);
-    GGML_ASSERT(token_left.find('\n')  == std::string::npos);
     GGML_ASSERT(token_right.find(' ')  == std::string::npos);
-    GGML_ASSERT(token_right.find('\n') == std::string::npos);
 
     auto it = pimpl->bpe_ranks.find(std::make_pair(token_left, token_right));
     if (it == pimpl->bpe_ranks.end()) {
