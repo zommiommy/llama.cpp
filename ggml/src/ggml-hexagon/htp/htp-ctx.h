e176905144173cc2ecf66b7098bca22d23973463
Shreya Jain <shreyajn@qti.qualcomm.com>
2026-04-21 14:16:04 -0700

    Hexagon: DAIG op (#22195)
    * hexagon: Add DIAG op
    
    * hexagon: add HVX support and DMA double buffering
    
    * hexagon: fix fatal error
    
    * hexagon: remove as many pragma(s) as possible

--- ggml/src/ggml-hexagon/htp/htp-ctx.h
+++ ggml/src/ggml-hexagon/htp/htp-ctx.h
@@ -98,5 +98,6 @@ int op_repeat(struct htp_ops_context * octx);
 int op_argsort(struct htp_ops_context * octx);
 int op_ssm_conv(struct htp_ops_context * octx);
 int op_cumsum(struct htp_ops_context * octx);
+int op_diag(struct htp_ops_context * octx);
 
 #endif /* HTP_CTX_H */
