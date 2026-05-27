7a511b00dfc4a8c6bf2549b0b8a571eb35158cba
Max Krasnyansky <maxk@qti.qualcomm.com>
2026-05-27 10:46:11 -0700

    hexagon: add support for Q4_1 in MUL_MAT and MUL_MAT_ID (#23647)
    * hex-mm: add support for Q4_1 matmul/matvec, hvx-only for now
    
    * hmx-mm: add support for Q4_1
    
    * hex-mm: use Q8_1 dynamic quantization to avoid having to compute sums in the vec_dot
    
    * hexagon: fix repack scratch buffer overflow
    
    * hex-mm: fix Q4_1 repack buffer sizing
    
    * hexagon: flip the build order for mm and fa (seems to help LTO)
    
    * hex-mm: add vec_dot 4x1s and minor HMX cleanup after adding Q4_1
    
    * hex-mm: fix fp16 vec_dot fallback to 2x1 and another issue that could cause incorrect output
    
    * hexagon: resurrect early-wake and add support for polling for op-batch completions
    
    With Q4_1 ggml-hexagon now claims pretty much the entire graphs which gives the CPU more time to chilax.
    This is a good thing! But it does add extra latency for the pure benchmark runs.
    Early wakeup helps recover the latency a bit in the normals runs and op-batch polling is just for benchmarking.
    
    ---------
    
    Co-authored-by: Todor Boinovski <todorb@qti.qualcomm.com>

--- scripts/snapdragon/adb/run-completion.sh
+++ scripts/snapdragon/adb/run-completion.sh
@@ -51,6 +51,9 @@ opbatch=
 opqueue=
 [ "$OQ" != "" ] && opqueue="GGML_HEXAGON_OPQUEUE=$OQ"
 
+oppoll=
+[ "$OP" != "" ] && oppoll="GGML_HEXAGON_OPPOLL=$OP"
+
 opflt=
 [ "$OF" != "" ] && opflt="GGML_HEXAGON_OPFILTER=$OF"
 
@@ -66,7 +69,7 @@ adb $adbserial $adbhost shell " \
   cd $basedir; ulimit -c unlimited;        \
     LD_LIBRARY_PATH=$basedir/$branch/lib   \
     ADSP_LIBRARY_PATH=$basedir/$branch/lib \
-    $verbose $sched $opmask $profile $nhvx $hmx $ndev $hb $opbatch $opqueue $opflt $vmem $mbuf \
+    $verbose $sched $opmask $profile $nhvx $hmx $ndev $hb $opbatch $opqueue $oppoll $opflt $vmem $mbuf \
       ./$branch/bin/llama-completion --no-mmap -m $basedir/../gguf/$model \
          --poll 1000 -t 6 --cpu-mask 0xfc --cpu-strict 1                  \
          --ctx-size 8192 --ubatch-size 1024 -fa on                        \
