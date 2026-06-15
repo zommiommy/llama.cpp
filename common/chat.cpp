41de2329922a1eee21be21bf8cf9f433817f3c91
Piotr Wilkin (ilintar) <piotr.wilkin@syndatis.com>
2026-06-15 17:33:54 +0200

    chat: include full unparsed prompt in debug (#24650)
    message on parse error

--- common/chat.cpp
+++ common/chat.cpp
@@ -2678,10 +2678,9 @@ common_chat_msg common_chat_peg_parse(const common_peg_arena &          src_pars
             }
             return msg;
         }
-        LOG_WRN("%s: unparsed %s output: %s\n", __func__, common_chat_format_name(params.format),
-                effective_input.substr(result.end).c_str());
-        throw std::runtime_error(std::string("The model produced output that does not match the expected ") +
-                                 common_chat_format_name(params.format) + " format");
+        LOG_WRN("%s: unparsed %s output: %s\n", __func__, common_chat_format_name(params.format), effective_input.substr(result.end).c_str());
+        LOG_DBG("%s: full %s output triggering error:\n=== BEGIN ===\n%s\n=== END ===\n", __func__, common_chat_format_name(params.format), effective_input.c_str());
+        throw std::runtime_error(std::string("The model produced output that does not match the expected ") + common_chat_format_name(params.format) + " format");
     }
 
     common_chat_msg msg;
